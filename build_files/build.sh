cb0e1522d01dc507a50d07781764a4bf33c77126
Matthew Gamble <git@matthewgamble.net>
2026-03-23 23:53:11 +1100

    Apparently /usr/local is a runtime symlink

--- build_files/build.sh
+++ build_files/build.sh
@@ -46,12 +46,12 @@ dnf5 -y install \
 
 # renovate: datasource=github-releases depName=djmattyg007/hr-zig
 HR_VERSION="v1.1.2"
-curl -fsSL "https://github.com/djmattyg007/hr-zig/releases/download/${HR_VERSION}/hr-zig_Linux_x86_64.tar.gz" | tar xz -C /usr/local/bin hr
+curl -fsSL "https://github.com/djmattyg007/hr-zig/releases/download/${HR_VERSION}/hr-zig_Linux_x86_64.tar.gz" | tar xz -C /var/usrlocal/bin hr
 
 # renovate: datasource=github-releases depName=regclient/regclient
 REGCLIENT_VERSION="v0.11.2"
-curl -fsSL -o /usr/local/bin/regctl "https://github.com/regclient/regclient/releases/download/${REGCLIENT_VERSION}/regctl-linux-amd64"
-curl -fsSL -o /usr/local/bin/regctl "https://github.com/regclient/regclient/releases/download/${REGCLIENT_VERSION}/regsync-linux-amd64"
+curl -fsSL -o /var/usrlocal/bin "https://github.com/regclient/regclient/releases/download/${REGCLIENT_VERSION}/regctl-linux-amd64"
+curl -fsSL -o /var/usrlocal/bin "https://github.com/regclient/regclient/releases/download/${REGCLIENT_VERSION}/regsync-linux-amd64"
 
 # Use a COPR Example:
 #
