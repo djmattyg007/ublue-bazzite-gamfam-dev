2e1da47d1634e6c9b71bd6fddd6780a7c81325ea
Matthew Gamble <git@matthewgamble.net>
2026-03-30 00:42:19 +1100

    Remove manual regclient bin installs
    These are now successfully managed by mise.

--- build-files/dnf-pre.sh
+++ build-files/dnf-pre.sh
@@ -5,10 +5,3 @@ set -exuo pipefail
 # renovate: datasource=github-releases depName=djmattyg007/hr-zig
 HR_VERSION="v1.1.2"
 curl -fsSL "https://github.com/djmattyg007/hr-zig/releases/download/${HR_VERSION}/hr-zig_Linux_x86_64.tar.gz" | tar xz -C /usr/bin hr
-
-# renovate: datasource=github-releases depName=regclient/regclient
-REGCLIENT_VERSION="v0.11.2"
-curl -fsSL -o /usr/bin/regctl "https://github.com/regclient/regclient/releases/download/${REGCLIENT_VERSION}/regctl-linux-amd64"
-chmod +x /usr/bin/regctl
-curl -fsSL -o /usr/bin/regsync "https://github.com/regclient/regclient/releases/download/${REGCLIENT_VERSION}/regsync-linux-amd64"
-chmod +x /usr/bin/regsync
