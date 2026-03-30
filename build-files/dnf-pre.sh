a081799ca7558520724fea6864661b7aac4aa740
Matthew Gamble <git@matthewgamble.net>
2026-03-30 12:17:29 +1100

    Install cargo-binstall outside of Mise
    It just seems like it's going to be cleaner and easier. Doing it this
    way also avoids a weird chicken and egg situation where Mise wants to
    use binstall to install other dependencies, but it has to somehow know
    to get binstall installed first.
    
    Also I was running into a weird situation where the shim for
    cargo-binstall just didn't work at all.

--- build-files/dnf-pre.sh
+++ build-files/dnf-pre.sh
@@ -5,3 +5,7 @@ set -exuo pipefail
 # renovate: datasource=github-releases depName=djmattyg007/hr-zig
 HR_VERSION="v1.1.2"
 curl -fsSL "https://github.com/djmattyg007/hr-zig/releases/download/${HR_VERSION}/hr-zig_Linux_x86_64.tar.gz" | tar xz -C /usr/bin hr
+
+# renovate: datasource=github-releases depName=cargo-bins/cargo-binstall
+CARGO_BINSTALL_VERSION="v1.17.9"
+curl -fsSL "https://github.com/cargo-bins/cargo-binstall/releases/download/${CARGO_BINSTALL_VERSION}/cargo-binstall-x86_64-unknown-linux-gnu.tgz" | tar xz -C /usr/bin cargo-binstall
