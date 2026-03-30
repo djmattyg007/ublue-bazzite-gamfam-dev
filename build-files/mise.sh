b4fb6dc735c8dea040ea424c380bf3f31a32f537
Matthew Gamble <git@matthewgamble.net>
2026-03-30 12:21:54 +1100

    Disable verbose mode for mise install
    I don't need it now.

--- build-files/mise.sh
+++ build-files/mise.sh
@@ -8,6 +8,6 @@ export MISE_STATE_DIR=/tmp/mise/state
 export MISE_TRUSTED_CONFIG_PATHS=/usr/share/mise
 mkdir -p /tmp/mise /var/cache/mise/{cache,data}
 
-MISE_DATA_DIR=/var/cache/mise/data mise install --verbose --cd /usr/share/mise --system
+MISE_DATA_DIR=/var/cache/mise/data mise install --cd /usr/share/mise --system
 
 MISE_DATA_DIR=/usr/share/mise/data mise reshim
