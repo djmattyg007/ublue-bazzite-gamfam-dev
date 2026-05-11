26f059f663dd4fe3a9c0fe09b4468462c00c3721
Matthew Gamble <git@matthewgamble.net>
2026-05-12 00:36:34 +1000

    Update cargo-binstall

--- build-files/dnf-pre.sh
+++ build-files/dnf-pre.sh
@@ -7,5 +7,5 @@ HR_VERSION="v1.1.2"
 curl -fsSL "https://github.com/djmattyg007/hr-zig/releases/download/${HR_VERSION}/hr-zig_Linux_x86_64.tar.gz" | tar xz -C /usr/bin hr
 
 # renovate: datasource=github-releases depName=cargo-bins/cargo-binstall
-CARGO_BINSTALL_VERSION="v1.17.9"
+CARGO_BINSTALL_VERSION="v1.19.1"
 curl -fsSL "https://github.com/cargo-bins/cargo-binstall/releases/download/${CARGO_BINSTALL_VERSION}/cargo-binstall-x86_64-unknown-linux-gnu.tgz" | tar xz -C /usr/bin cargo-binstall
